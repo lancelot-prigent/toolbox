aa6a69e0816d513763d89c8e849364504980b6a7
Lancelot Prigent <lancelot@prigent.dev>
2025-06-27 17:20:04 +0200

    feat: Enhance README and add Infrastructure as Code setup
    - Updated README.md with detailed setup instructions for Scaleway and GitHub.
    - Added iac-plan GitHub Actions workflow for Terraform infrastructure planning.
    - Introduced Terraform configuration files for Scaleway backend and provider.

---
+++ infrastructure/core/backend.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket                      = "tbox-terraform-state"
+    key                         = "core.tfstate"
+    region                      = "fr-par"
+    endpoint                    = "https://s3.fr-par.scw.cloud"
+    access_key                  = ""
+    secret_key                  = ""
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    use_lockfile                = true
+  }
+}
