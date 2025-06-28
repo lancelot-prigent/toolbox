02c4d208df8a3d77579c3f20ab800055f3db6246
Lancelot Prigent <lancelot@prigent.dev>
2025-06-28 06:59:22 +0200

    fix: Refactor output variable names in preview-determine-actions script
    - Updated output variable names to camelCase for consistency with JavaScript conventions.
    - Simplified state extraction logic for better readability.
    - Removed unnecessary console logs to streamline the script.

--- .github/scripts/preview-determine-actions.js
+++ .github/scripts/preview-determine-actions.js
@@ -2,16 +2,13 @@ const { getPrState, getPrLabels } = require('./utils');
 
 module.exports = async ({ github, context, core }) => {
   const labels = await getPrLabels(context);
-  const state = await getPrState(github, context);
+  const { state } = (await getPrState(github, context)) || {};
 
   const shouldDeploy = labels.includes('preview');
   const shouldApply = !state && shouldDeploy;
-  const shouldDestroy = state  && !shouldDeploy;
+  const shouldDestroy = !!state && !shouldDeploy;
 
-  console.log({ shouldApply, shouldDestroy, shouldDeploy });
-  console.log(JSON.stringify({ context, github }, null, 2));
-
-  core.setOutput('should-apply', shouldApply);
-  core.setOutput('should-destroy', shouldDestroy);
-  core.setOutput('should-deploy', shouldDeploy);
+  core.setOutput('shouldApply', shouldApply);
+  core.setOutput('shouldDestroy', shouldDestroy);
+  core.setOutput('shouldDeploy', shouldDeploy);
 }
