1bd1f6eac07d75e8733cafda2ce0d9df301838cd
Lancelot Prigent <lancelot@prigent.dev>
2025-06-28 13:22:49 +0200

    fix: Update preview-determine-actions script and workflow for improved deployment logic
    - Refactored the preview-determine-actions.js script to adjust deployment conditions based on pull request actions.
    - Removed unnecessary output variable for should-apply in the preview-deploy.yml workflow.
    - Enhanced clarity in the workflow by simplifying condition checks for deployment and destruction actions.

--- .github/scripts/preview-determine-actions.js
+++ .github/scripts/preview-determine-actions.js
@@ -4,11 +4,9 @@ module.exports = async ({ github, context, core }) => {
   const labels = await getPrLabels(context);
   const state = await getPrState(github, context);
 
-  const shouldDeploy = labels.includes('preview');
-  const shouldApply = !state && shouldDeploy;
-  const shouldDestroy = !!state && !shouldDeploy;
+  const shouldDeploy = labels.includes('preview') && context.payload.action !== 'closed';
+  const shouldDestroy = context.payload.action === 'closed' || (!!state && !shouldDeploy);
 
-  core.setOutput('should-apply', shouldApply);
   core.setOutput('should-destroy', shouldDestroy);
   core.setOutput('should-deploy', shouldDeploy);
 }
