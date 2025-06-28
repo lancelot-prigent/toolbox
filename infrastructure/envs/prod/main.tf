db68a2b0b72873d0769a867321196bf135880b90
Lancelot Prigent <lancelot@prigent.dev>
2025-06-28 07:45:10 +0200

    fix: Update environment configurations in main.tf files
    - Removed redundant provider configurations for Scaleway and GitHub.
    - Added GitHub token and owner parameters directly to the environment module for clarity and maintainability.
    - Ensured consistency across all environment configurations (dev, preview, prod, staging).

--- infrastructure/envs/prod/main.tf
+++ infrastructure/envs/prod/main.tf
@@ -1,13 +1,8 @@
-provider "scaleway" {}
-
-provider "github" {
-  token = var.gh_pat_token
-  owner = var.gh_owner
-}
-
 module "environment" {
   source = "../../modules/environment"
 
   env_name     = "prod"
   gh_repo_name = var.gh_repo_name
+  gh_pat_token = var.gh_pat_token
+  gh_owner     = var.gh_owner
 }
