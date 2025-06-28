1c65246b65ac80571f78665d122936c666ff89aa
Lancelot Prigent <lancelot@prigent.dev>
2025-06-28 07:59:46 +0200

    fix: Add validation for GitHub environment variables
    - Implemented validation for GitHub repository name, personal access token, and owner to ensure they are provided.
    - Enhanced error messages for clarity and user guidance during configuration.

--- infrastructure/modules/environment/variables.tf
+++ infrastructure/modules/environment/variables.tf
@@ -21,15 +21,30 @@ variable "env_name" {
 variable "gh_repo_name" {
   type        = string
   description = "GitHub repository name"
+
+  validation {
+    condition     = length(var.gh_repo_name) > 0
+    error_message = "GitHub repository name must be provided"
+  }
 }
 
 variable "gh_pat_token" {
   type        = string
   description = "GitHub personal access token with permissions over the repository"
   sensitive   = true
+
+  validation {
+    condition     = length(var.gh_pat_token) > 0
+    error_message = "GitHub personal access token must be provided"
+  }
 }
 
 variable "gh_owner" {
   type        = string
   description = "GitHub owner"
+
+  validation {
+    condition     = length(var.gh_owner) > 0
+    error_message = "GitHub owner must be provided"
+  }
 }
